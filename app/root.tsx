c47c7363b9fa5bb069b3d2fa3828180fa283fc7e
lifeisegg123 <leejj2002@naver.com>
2024-05-26 11:49:04 +0900

    chore: edit html

--- app/root.tsx
+++ app/root.tsx
@@ -28,10 +28,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <html lang="en">
+    <html lang="ko">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Peacock</title>
         <Meta />
         <Links />
       </head>
