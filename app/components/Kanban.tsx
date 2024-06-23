ddf750b04696f120e1b5e0dd4cd7b15d634b71c5
lifeisegg123 <leejj2002@naver.com>
2024-06-23 17:01:47 +0900

    feat: add kanban example

---
+++ app/components/Kanban.tsx
@@ -0,0 +1,160 @@
+import { DragEndEvent, DragOverEvent } from "@dnd-kit/core";
+import {
+  arrayMove,
+  horizontalListSortingStrategy,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { startTransition, useState } from "react";
+import { css } from "styled-system/css";
+import { hstack, vstack } from "styled-system/patterns";
+import { DnD } from "./Dnd";
+import * as R from "remeda";
+
+const dndItems = Array.from({ length: 3 }).map((_, i) => ({
+  id: `${i}`,
+  values: Array.from({ length: 5 }).map((_, ii) => ({
+    id: `${i}_${ii}`,
+    value: `${i * 10 + ii}번 item`,
+  })),
+}));
+
+export function Kanban() {
+  const [dnd, setDnd] = useState(dndItems);
+
+  const findColumn = (id: string | null) =>
+    id ? dnd.find((col) => col.values.find((task) => task.id === id)) : null;
+
+  const handleDragOver = (event: DragOverEvent) => {
+    const { active, over, delta } = event;
+    const activeId = String(active.id);
+    const overId = over ? String(over.id) : null;
+
+    const activeColumn = findColumn(activeId);
+    const overColumn = findColumn(overId);
+    if (!activeColumn || !overColumn || activeColumn === overColumn) {
+      return null;
+    }
+    startTransition(() => {
+      setDnd((prevState) => {
+        const activeItems = activeColumn.values;
+        const overItems = overColumn.values;
+        const activeIndex = activeItems.findIndex((i) => i.id === activeId);
+        const overIndex = overItems.findIndex((i) => i.id === overId);
+
+        const newIndex = () => {
+          if (overIndex === 0) {
+            return delta.y < 0 ? overIndex : overIndex + 1;
+          } else if (overIndex === overItems.length - 1 && delta.y > 0) {
+            return overIndex + 1;
+          } else {
+            return overIndex >= 0 ? overIndex : overItems.length;
+          }
+        };
+
+        return prevState.map((column) => {
+          if (column.id === activeColumn.id) {
+            return {
+              ...column,
+              values: activeItems.filter((i) => i.id !== activeId),
+            };
+          } else if (column.id === overColumn.id) {
+            const newInsertIndex = newIndex();
+            return {
+              ...column,
+              values: [
+                ...overItems.slice(0, newInsertIndex),
+                activeItems[activeIndex],
+                ...overItems.slice(newInsertIndex, overItems.length),
+              ],
+            };
+          } else {
+            return column;
+          }
+        });
+      });
+    });
+  };
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    console.log(active, over);
+    const activeId = String(active.id);
+    const overId = over ? String(over.id) : null;
+    const activeType = active.data.current?.type;
+    if (activeType === "Lane") {
+      const activeIndex = dnd.findIndex((v) => v.id === activeId);
+      const overIndex = dnd.findIndex((v) => v.id === overId);
+      setDnd((prev) => arrayMove(prev, activeIndex, overIndex));
+    } else {
+      const activeColumn = findColumn(activeId);
+      const overColumn = findColumn(overId);
+      if (!activeColumn || !overColumn || activeColumn !== overColumn) {
+        return null;
+      }
+      const activeIndex = activeColumn.values.findIndex(
+        (i) => i.id === activeId
+      );
+      const overIndex = overColumn.values.findIndex((i) => i.id === overId);
+      if (activeIndex !== overIndex) {
+        setDnd((prevState) => {
+          return prevState.map((column) => {
+            if (column.id === activeColumn.id) {
+              return {
+                id: column.id,
+                values: arrayMove(overColumn.values, activeIndex, overIndex),
+              };
+            } else {
+              return column;
+            }
+          });
+        });
+      }
+    }
+  };
+
+  return (
+    <DnD onDragOver={handleDragOver} onDragEnd={handleDragEnd}>
+      <ul className={hstack({ alignItems: "stretch", height: "240" })}>
+        <DnD.SortableRoot items={dnd} strategy={horizontalListSortingStrategy}>
+          {dnd.map((v) => (
+            <DnD.Sortable
+              className={css({ height: "100%", overflow: "auto" })}
+              key={v.id}
+              sortableOptions={{ id: v.id, data: { type: "Lane" } }}
+            >
+              <ul
+                className={vstack({
+                  background: "BG/Background",
+                  color: "Black/10",
+                  p: "8",
+                  borderRadius: "4",
+                })}
+              >
+                <div>{v.id} 열</div>
+                <DnD.SortableRoot
+                  items={v.values}
+                  strategy={verticalListSortingStrategy}
+                >
+                  {v.values.map((vv) => (
+                    <DnD.Sortable
+                      key={vv.id}
+                      sortableOptions={{
+                        id: vv.id,
+                        data: { type: "Task" },
+                      }}
+                      className={css({
+                        p: "8",
+                      })}
+                    >
+                      {vv.value}
+                    </DnD.Sortable>
+                  ))}
+                </DnD.SortableRoot>
+              </ul>
+            </DnD.Sortable>
+          ))}
+        </DnD.SortableRoot>
+      </ul>
+    </DnD>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,18 @@
+import { DragOverEvent } from "@dnd-kit/core";
+import {
+  arrayMove,
+  horizontalListSortingStrategy,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 import { type MetaFunction } from "@remix-run/node";
+import { useState } from "react";
+import { css } from "styled-system/css";
+import { hstack, vstack } from "styled-system/patterns";
 import { DatePicker } from "~/components/DatePicker";
+import { DnD } from "~/components/Dnd";
 import { FormField } from "~/components/FormField";
 import { InputBox } from "~/components/Input";
+import { Kanban } from "~/components/Kanban";
 import { Select } from "~/components/Select";
 import { Combobox } from "~/components/TagComboBox";
 
@@ -17,67 +28,75 @@ export default function Index() {
     <div>
       <h1>Welcome to Remix</h1>
 
-      <DatePicker>
-        <DatePicker.Control>
-          <DatePicker.Label>테스트 date picker</DatePicker.Label>
+      <div className={hstack()}>
+        <span>
+          <DatePicker>
+            <DatePicker.Control>
+              <DatePicker.Label>테스트 date picker</DatePicker.Label>
+              <InputBox>
+                <InputBox.Content asChild>
+                  <DatePicker.Trigger>
+                    <InputBox.Input readOnly />
+                  </DatePicker.Trigger>
+                </InputBox.Content>
+              </InputBox>
+              <FormField.ErrorMessage />
+            </DatePicker.Control>
+            <DatePicker.Content>
+              <DatePicker.DayView>
+                <DatePicker.Navigator />
+                <DatePicker.Table>
+                  <DatePicker.DayViewHead></DatePicker.DayViewHead>
+                  <DatePicker.DayViewBody
+                    renderCell={(day) => (
+                      <DatePicker.DayViewCell day={day}>
+                        ??
+                      </DatePicker.DayViewCell>
+                    )}
+                  />
+                </DatePicker.Table>
+              </DatePicker.DayView>
+            </DatePicker.Content>
+          </DatePicker>
+
+          <Select items={["1", "2", "3"]}>
+            <Select.Control>
+              <Select.Label>테스트 셀렉트</Select.Label>
+              <Select.FieldBox placeholder="test123" />
+            </Select.Control>
+            <Select.Content>
+              <Select.ItemGroup />
+            </Select.Content>
+          </Select>
+
           <InputBox>
-            <InputBox.Content asChild>
-              <DatePicker.Trigger>
-                <InputBox.Input readOnly />
-              </DatePicker.Trigger>
+            <InputBox.Label>테스트 인풋 필드</InputBox.Label>
+            <InputBox.Content>
+              <InputBox.Input placeholder="테스트 인풋 필드" />
+              <InputBox.Clear />
             </InputBox.Content>
           </InputBox>
-          <FormField.ErrorMessage />
-        </DatePicker.Control>
-        <DatePicker.Content>
-          <DatePicker.DayView>
-            <DatePicker.Navigator />
-            <DatePicker.Table>
-              <DatePicker.DayViewHead></DatePicker.DayViewHead>
-              <DatePicker.DayViewBody
-                renderCell={(day) => (
-                  <DatePicker.DayViewCell day={day}>??</DatePicker.DayViewCell>
-                )}
-              />
-            </DatePicker.Table>
-          </DatePicker.DayView>
-        </DatePicker.Content>
-      </DatePicker>
 
-      <Select items={["1", "2", "3"]}>
-        <Select.Control>
-          <Select.Label>테스트 셀렉트</Select.Label>
-          <Select.FieldBox placeholder="test123" />
-        </Select.Control>
-        <Select.Content>
-          <Select.ItemGroup />
-        </Select.Content>
-      </Select>
-
-      <InputBox>
-        <InputBox.Label>테스트 인풋 필드</InputBox.Label>
-        <InputBox.Content>
-          <InputBox.Input placeholder="테스트 인풋 필드" />
-          <InputBox.Clear />
-        </InputBox.Content>
-      </InputBox>
-
-      <Combobox items={items} multiple>
-        <Combobox.Control>
-          <Combobox.Label>테스트 콤보박스</Combobox.Label>
-          <Combobox.Trigger asChild>
-            <Combobox.TagInputBox></Combobox.TagInputBox>
-          </Combobox.Trigger>
-        </Combobox.Control>
-        <Combobox.Content>
-          <Combobox.ItemGroup />
-        </Combobox.Content>
-      </Combobox>
-
-      {/* <Multiple /> */}
+          <Combobox items={items} multiple>
+            <Combobox.Control>
+              <Combobox.Label>테스트 콤보박스</Combobox.Label>
+              <Combobox.Trigger asChild>
+                <Combobox.TagInputBox></Combobox.TagInputBox>
+              </Combobox.Trigger>
+            </Combobox.Control>
+            <Combobox.Content>
+              <Combobox.ItemGroup />
+            </Combobox.Content>
+          </Combobox>
+        </span>
+        <span>
+          <Kanban />
+        </span>
+      </div>
     </div>
   );
 }
+
 const items = [
   { label: "React", value: "react" },
   { label: "Solid", value: "solid" },
